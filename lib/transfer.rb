fe3a5da431026022c2918ec87b0cdd7e4f05fc7a
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 21:35:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,5 +3,8 @@ class Transfer
   attr_accessor :sender, :receiver, :status, :amount
   
   def initialize(sender, receiver, status)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
   end
 end
