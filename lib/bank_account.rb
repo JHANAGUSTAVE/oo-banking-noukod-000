964de08456a694c0b0c5ca8d14ce7f09057b5e7c
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 21:27:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,9 @@
 class BankAccount
-  attr_accessor :name
-def initialize(name)
-  @name = name
+  attr_accessor :balance, :status
+  attr_reader :name
+def initialize(acount_name)
+  @name = acount_name
+  balance = 1000
+  status = "open"
 end
 end
