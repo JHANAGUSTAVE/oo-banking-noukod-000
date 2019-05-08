763ade90784cc91205725248fafc3c7e3b41c3d9
Jhana Gustave <jhana.gustave@gmail.com>
2019-05-08 21:31:06 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -12,5 +12,14 @@ class BankAccount
     end
     
     def display_balance
-    end
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    @status == "open" && @balance > 0 ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
